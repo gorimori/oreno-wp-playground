9ca6c6366e4176d29d6460315a92100d895741ba
gorimori <34891695+gorimori@users.noreply.github.com>
2020-03-06 00:12:45 +0900

    update Picture

--- public/wp-content/theme/oreno-theme/Picture.php
+++ public/wp-content/theme/oreno-theme/Picture.php
@@ -2,24 +2,49 @@
 
 class Picture
 {
+  /** /img/hoge/foo */
   private string $path;
+  private bool $need_lazyload = true;
 
-  function __construct(string $path)
+  function __construct(string $path, bool $need_lazyload = true)
   {
     $this->path = $path;
+    $this->need_lazyload = $need_lazyload;
+  }
+
+  private function src_attr(): string
+  {
+    if ($this->need_lazyload) {
+      return 'data-src';
+    }
+    return 'src';
+  }
+
+  private function srcset_attr(): string
+  {
+    if ($this->need_lazyload) {
+      return 'data-srcset';
+    }
+    return 'srcset';
   }
 
   function source(string $type): string
   {
+    $srcset = $this->srcset_attr();
+    $srcset_value = sprintf("%s, %s 2x", $this->path . '.' . $type, $this->path . '@2x.' . $type);
     return <<< HTML
-<source srcset="" type="image/${type}">
+<source ${srcset}="${srcset_value}" type="image/${type}">
 HTML;
   }
 
-  function img(string $type): string
+  function img(string $type, string $alt = ''): string
   {
+    $src = $this->src_attr();
+    $srcset = $this->srcset_attr();
+    $src_value = $this->path . '.' . $type;
+    $srcset_value = sprintf("%s, %s 2x", $src_value, $this->path . '@2x.' . $type);
     return <<< HTML
-<img src="" alt="" srcset="">
+<img ${src}="${src_value}" alt="${alt}" ${srcset}="$srcset_value">
 HTML;
   }
 }
